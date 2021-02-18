fa2450aa04d8c3a008d0aee97029d2f05688a784
Davis McGregor <davisjm2@illinois.edu>
2021-02-18 11:08:26 -0600

    code to play with

---
+++ learning/__init__.py
@@ -0,0 +1,10 @@
+"""
+This is the top level __init__ (or initialization) file.
+Nothing really happens here, but you need the __init__.py file to be present.
+Most people use this to introduce their package ('learning' in this case)
+and any subpackages. Here, we just have one file, no subpackages.
+
+At the bottom, I have included the __version__ variable. This information is 
+used by the setup.py file at the root of the repository.
+"""
+__version__ = '0.1.0'
---
+++ learning/mistakes.py
@@ -0,0 +1,27 @@
+def joke():
+    '''
+    This is called a docstring. It explains
+    exactly what the function does.
+    '''
+    return ('Getting a PhD is easy')
+
+
+def add(a, b):
+    '''
+    Add two variables together
+
+    Parameters
+    ----------
+    a : float
+        First number to add
+    b : float
+        Second number to add
+    
+    Returns
+    -------
+    c : float
+        Sum of a and b
+    '''
+    # TODO: generalize so we can add more than floats
+    c = a - b   # oops, fix this mistake
+    return c
